93c40635d150265400759e2495c9ab7e6f93cac6
Suphawit <112681287+suphawit1@users.noreply.github.com>
2025-01-01 00:11:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ function onWin() {
 
     // Create the text below the image
     const text = document.createElement('p');
-    text.textContent = 'Happy New Year. Doge กำลังไปเอาของขวัญให้';
+    text.textContent = 'Happy New Year. แต่ Doge ไม่มีของขวัญให้แล้ว T_T';
     text.style.color = 'white';
     text.style.marginTop = '10px';
     text.style.fontSize = '18px';
@@ -287,7 +287,7 @@ function onWin() {
 
     // Wait 5 seconds and redirect
     setTimeout(() => {
-        window.location.href = 'https://gift.truemoney.com/campaign/?v=01941d91485e7b3883d51c49451e6f139bX'; // Redirect URL
+        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // Redirect URL
     }, 5000);
 }
 
